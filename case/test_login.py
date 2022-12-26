c3280d816441f8c84e78ddb66638ae4fb5dd24a9
sp1 <sp1@meitu.com>
2022-12-26 12:41:09 +0800

    代码提交

--- case/test_login.py
+++ case/test_login.py
@@ -19,20 +19,31 @@ class Test_Login(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver = base_config.init_driver()
+        base_operate.base_login(cls.driver)
 
     def test_login(self):
-        base_operate.enter_home(self.driver)
-        self.driver.find_element(By.XPATH, '//android.widget.ImageView[@content-desc="登录"]').click()
-        time.sleep(1)
-        if base_operate.is_element_exist(self.driver, '一键登录'):
-            self.driver.find_element(By.XPATH, '//*[@resource-id="com.zhihuidanji.smarterlayer:id/title"]').click()
-        time.sleep(3)
-        self.driver.find_element(By.XPATH, '//android.widget.ImageView[contains(@index,7)]').click()
-        time.sleep(1)
-        user_name = self.driver.find_element(By.XPATH, '//android.widget.EditText[@text="请输入手机号"]')
-        user_name.click()
-        os.system('adb shell input text {}'.format('13699103657'))
-        password = self.driver.find_element(By.XPATH, '//android.widget.EditText[@text="请输入密码"]')
-        password.click()
-        os.system('adb shell input text {}'.format('123456'))
-        self.driver.find_element(By.XPATH, '//android.widget.ImageView[contains(@index,3)]').click()
+        try:
+            self.driver.find_element(By.XPATH, '//android.widget.ImageView[@content-desc="登录"]').click()
+            time.sleep(1)
+            if base_operate.is_element_exist(self.driver, '一键登录'):
+                # self.driver.find_element(By.XPATH, '//*[@resource-id="com.zhihuidanji.smarterlayer:id/title"]').click()
+                self.driver.find_element(By.XPATH,
+                                         '//*[@resource-id="com.zhihuidanji.smarterlayer:id/yd_quick_login_privacy_checkbox"]').click()
+                time.sleep(1)
+                self.driver.find_element(By.XPATH,
+                                         '//*[@resource-id="com.zhihuidanji.smarterlayer:id/oauth_login"]').click()
+            else:
+                time.sleep(3)
+                self.driver.find_element(By.XPATH, '//android.widget.ImageView[contains(@index,7)]').click()
+                time.sleep(3)
+                user_name = self.driver.find_element(By.XPATH, '//android.widget.EditText[@text="请输入手机号"]')
+                user_name.click()
+                os.system('adb shell input text {}'.format('17338123926'))
+                time.sleep(2)
+                password = self.driver.find_element(By.XPATH, '//android.widget.EditText[@text="请输入密码"]')
+                password.click()
+                os.system('adb shell input text {}'.format('123456'))
+                self.driver.find_element(By.XPATH, '//android.widget.ImageView[contains(@index,3)]').click()
+
+        finally:
+            self.driver.close_app()
